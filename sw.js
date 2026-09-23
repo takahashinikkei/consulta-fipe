156509fcc9660d9d69111d758ca48beb3c1cfd60
takahashinikkei <dehtakahashi@gmail.com>
2026-09-23 17:24:19 -0300

    Add files via upload
    Atualiza histórico FIPEX v16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='consulta-fipe-v1';
+const CACHE='consulta-fipe-v16';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))) });
