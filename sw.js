5aaa1090a8690272da2e96b8697feede55ed48ea
takahashinikkei <dehtakahashi@gmail.com>
2026-09-23 22:31:07 -0300

    Atualiza cache para v25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='consulta-fipe-v24';
+const CACHE='consulta-fipe-v25';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))) });
