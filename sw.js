89ddcd31933124b4f0a994dc54ced12c6df1636a
takahashinikkei <dehtakahashi@gmail.com>
2026-09-24 16:41:07 -0300

    enable Garagem 26 PWA offline shell

--- sw.js
+++ sw.js
@@ -1,12 +1,19 @@
-const CACHE='consulta-fipe-v37';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
- if(e.request.method!=='GET')return;
- const isPage=e.request.mode==='navigate'||e.request.destination==='document';
- e.respondWith(
-  fetch(e.request,{cache:isPage?'no-store':'default'}).then(r=>{
-   const c=r.clone(); caches.open(CACHE).then(x=>x.put(e.request,c)); return r;
-  }).catch(()=>caches.match(e.request))
+const CACHE='garagem26-v40';
+const APP_SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>{
+ event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+ event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+ if(event.request.method!=='GET')return;
+ const isNavigation=event.request.mode==='navigate';
+ event.respondWith(
+  fetch(event.request,{cache:isNavigation?'no-store':'default'}).then(response=>{
+   const copy=response.clone();
+   caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+   return response;
+  }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html')))
  );
 });
