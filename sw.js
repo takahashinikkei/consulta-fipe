c7550fc43c3c41f8df49bce976fc3d892862e391
takahashinikkei <dehtakahashi@gmail.com>
2026-09-23 23:23:42 -0300

    Forca atualizacao do app e cache v33

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-const CACHE='consulta-fipe-v32';
+const CACHE='consulta-fipe-v33';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))) });
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ const isPage=e.request.mode==='navigate'||e.request.destination==='document';
+ e.respondWith(
+  fetch(e.request,{cache:isPage?'no-store':'default'}).then(r=>{
+   const c=r.clone(); caches.open(CACHE).then(x=>x.put(e.request,c)); return r;
+  }).catch(()=>caches.match(e.request))
+ );
+});
