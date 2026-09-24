40ee929864fd6552fd229e6665563a699f1e053d
takahashinikkei <dehtakahashi@gmail.com>
2026-09-23 23:32:54 -0300

    Atualiza cache para v35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='consulta-fipe-v34';
+const CACHE='consulta-fipe-v35';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
